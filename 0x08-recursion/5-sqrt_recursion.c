afef814200a636eb8b82c76a5324e445e6750dc7
Emo-Efe <judeaustine1@gmail.com>
2023-04-06 00:28:54 -0700

    a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,21 +9,5 @@ int _sqrt_recursion(int n)
 {
 	if (n - 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
-}
-
-/**
- * actual_sqrt_recursion - recurses to find the natural
- * squar root of a number
- * @n: number tocalculate the squar root of
- * @i: Iterator
- * Return: the resulting squar root
- */
-int actual_sqrt_recursion(int n, int i)
-{
-	if (i * i > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	return (_sqrt_recursion(n, 0));
 }
