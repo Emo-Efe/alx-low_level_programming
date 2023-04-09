32a1d53b12aafd2f60fedcca191e6bb211b47e56
Emo-Efe <judeaustine1@gmail.com>
2023-04-09 09:42:05 -0700

    a program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the porogram
+ * @argc: number of arguments
+ * @argv: array of the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
