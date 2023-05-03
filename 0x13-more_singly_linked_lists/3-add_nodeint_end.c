471811a7479805e63e4c35e80be30ab22d27a5f1
Emo-Efe <judeaustine1@gmai.com>
2023-05-03 01:33:56 -0700

    3. Add node at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,11 +10,12 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node, *last_node;
+	listint_t *new_node;
+	listint_t *temp = *head;
 
 	/* Create a new node */
 	new_node = mallock(sizeof(list_t));
-		if (new_node == NULL)
+		if (!new_node)
 			return (NULL);
 
 		/* let's initialize the new node */
@@ -29,10 +30,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 
 	/* Else, find the last node and append the new node */
-	last_node = *head;
-	while (last_node->next != NULL)
-		last_node = last_node->next;
-	last_node->next = new_node;
+
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_node;
 
 	return (new_node);
 }
