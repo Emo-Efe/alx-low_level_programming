3feb0d5cb1152d25f4c14e7aa37c3859ba7548a4
Emo-Efe <judeaustine1@gmai.com>
2023-05-04 18:30:16 -0700

    5.101

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,16 +9,18 @@
  * Return:the  number of bits to change
  */
 
-unsigned int flip_bits(unsigned long int n, unsigned long int m) {
-    unsigned int count = 0;
-    unsigned long int exclusive = n ^ m;
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
-    for (int i = 63; i >= 0; i--) {
-        unsigned long int current = exclusive >> i;
-        if (current & 1) {
-            count++;
-        }
-    }
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
 
-    return count;
+	return (count);
 }
