30741f7fc52f51c38bb2b9f45f2d07fc9ff25c66
Emo-Efe <judeaustine1@gmail.com>
2023-07-14 14:15:10 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,21 +11,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd;
-	ssize_t w;
-	ssize_t r;
+	int fd;
+	ssize_t rd, wr;
 	char *buf;
+
+	if (!filename)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	r = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, r);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	rd = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	close(fd);
 
 	free(buf);
-	close(fd);
-	return (w);
+
+	return (wr);
 }
