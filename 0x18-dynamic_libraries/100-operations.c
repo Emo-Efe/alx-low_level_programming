0c7df87d3440108a24fe26a6837507ec8c99b767
Emo-Efe <judeaustine1@gmail.com>
2023-08-21 21:33:03 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+	return (a + b);
+}
+int sub(int a, int b)
+{
+	return (a - b);
+}
+int mul(int a, int b)
+{       
+	int product = (a * b);
+	return (product);
+}
+int div(int a, int b)
+{
+	return (a / b);
+}
+int mod(int a, int b)
+{
+	return (a % b);
+}
