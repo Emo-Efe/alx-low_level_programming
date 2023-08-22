c4cb606804255659d2d442dcc339d8df04986a17
Emo-Efe <judeaustine1@gmail.com>
2023-08-21 20:23:57 -0700

    Task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,5 @@
-//AUTH: EMO-EFE
 #!/bin/bash
-gcc -Wall Wextra -Werror -pedantic -c -fPIC *.c
+
+gcc -Wall -pedantic -Werror -Wextra -L -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
--- 0x18-dynamic_libraries/4-isalpha.c
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -10,8 +10,13 @@
 
 int _isalpha(int c)
 {
-	if(( c >= 97 && c <= 122) || c >= 65 && c <= 90)
-	 return (1);
+	if ((c >= 'A' && c <= 'Z') ||
+	(c >= 'a' && c <= 'z'))
+	{
+	return (1);
+	}
 	else
+	{
 	return (0);
+	}
 }
